27b844a3017b63a68f34e11dc88ae08c09af65ac
Finn Pryor <finn@theofficialswat.com>
2013-05-23 10:44:55 -0400

    Created Rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SWATbook::Application.config.secret_token = 'b08f691938a8b119e07efea32badbb27c5ee8579644f9f5f73ea25e32545302f3da1b1696e2b59b5fabbdf6bbbb85f418f9e92cbd745f2edc59e031a747a4047'
